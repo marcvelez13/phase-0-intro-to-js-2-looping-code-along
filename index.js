d4b9e235955626f4d697ae213bff20d0bf4eca2c
Marc Velez <marcvelez13@gmail.com>
2022-05-24 15:55:59 -0400

    commit tree 7f8b491ef25051562b212878eb50298a3c813274

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solutions in this file
+const names = (['value1','value2','value3']);
+function writeCards(names,){
+    let i = 0; i++;
+    while(names < names.legth){
+    console.log(`Thank you, ${names[i]}, for the wonderful suprise gift! `);
+    debugger;
+}
+return writeCards;
+}
+
+function countdown(){
+     const countdown = 0
+    let i = 0; i--;
+    while(i <= 10);
+    console.log(countdown[i]);
+}
